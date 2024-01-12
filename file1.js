d2dbbae272e474b1b3a90991116a53e7ac139c9c
nataliasoshenko <151614890+nataliasoshenko@users.noreply.github.com>
2024-01-12 19:35:07 +0400

    commit 1

---
+++ file1.js
@@ -0,0 +1,7 @@
+"use strict";
+function greeter(person) {
+    return "Hello, " + person;
+}
+let user = "Jane User";
+document.body.textContent = greeter(user);
+//# sourceMappingURL=file1.js.map
---
+++ file2.js
@@ -0,0 +1,4 @@
+"use strict";
+let user2 = "Jane User";
+document.body.textContent = user2;
+//# sourceMappingURL=file2.js.map
